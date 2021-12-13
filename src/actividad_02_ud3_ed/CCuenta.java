b885fb8d641957b2486e13451019cbd99102c2b0
sergiomateos1daw <smatpue573@g.educaand.es>
2021-12-13 14:56:42 +0100

    Revision 2 | Genera JavaDoc

---
+++ src/actividad_02_ud3_ed/CCuenta.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actividad_02_ud3_ed;
+
+/**
+ * 
+ * @author Ana
+ */
+public class CCuenta {
+
+/**
+ * AQUÍ ESTAMOS DECLARANDO LAS VARIABLES
+ * @author Sergio
+ * @version 1.0.0
+ */
+     
+  protected String nombre;
+    private String cuenta;
+    private double saldo;
+    private double tipoInterés;
+
+  
+    public CCuenta ()
+    {
+    }
+   /**
+    * 
+    * @param nom Nombre del titular de la cuenta
+    * @param cue Numero de cuenta
+    * @param sal Cantidad de dinero en la cuenta
+    * @param tipo Tipo de cuenta
+    */
+    public CCuenta (String nom, String cue, double sal, double tipo)
+    {
+        nombre =nom;
+        cuenta=cue;
+        saldo=sal;
+        tipoInterés=tipo;
+    }
+  /**
+   * 
+   * @param nom Nombre del titular de la cuenta
+   */
+    public void asignarNombre(String nom)
+    {
+        nombre=nom;
+    }
+   /**
+    * 
+    * @return nombre del titular de la cuenta 
+    */
+    public String obtenerNombre()
+    {
+        return nombre;
+    }
+    
+    /**
+     * 
+     * @return saldo del saldo que tiene la cuenta
+     */
+    
+     public double estado ()
+    {
+        return saldo;
+    }
+
+    
+    public void ingresar(double cantidad) throws Exception
+    {
+        if (cantidad<0){
+            throw new Exception("No se puede ingresar una cantidad negativa");}
+        setSaldo(saldo + cantidad);
+    }
+
+   
+    public void retirar (double cantidad) throws Exception
+    {
+        if (cantidad < 0){
+            throw new Exception ("No se puede retirar una cantidad negativa");}
+        if (estado()< cantidad){
+            throw new Exception ("No se hay suficiente saldo");}
+        setSaldo(saldo - cantidad);
+    }
+    
+    public String obtenerCuenta ()
+    {
+        return cuenta;
+    }
+
+  
+  public void setCuenta(String cuenta) {
+    this.cuenta = cuenta;
+  }
+
+  
+  public void setSaldo(double saldo) {
+    this.saldo = saldo;
+  }
+
+ 
+  public double getTipoInterés() {
+    return tipoInterés;
+  }
+
+  
+  public void setTipoInterés(double tipoInterés) {
+    this.tipoInterés = tipoInterés;
+  }
+}
---
+++ src/actividad_02_ud3_ed/Main.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package actividad_02_ud3_ed;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class Main {
+    static BufferedReader dato = new BufferedReader(new InputStreamReader(System.in));
+
+   
+    public static void main(String[] args) {
+        CCuenta cuenta1;
+        double saldoActual;
+        int opcion=0;
+        cuenta1 = new CCuenta("Juan López","1000-2365-85-123456789",2500,0);
+        do{
+            try {
+                System.out.println("MENÚ DE OPERACIONES");
+                System.out.println("-------------------");
+                System.out.println("1 - Ingresar");
+                System.out.println("2 - Retirar");
+                System.out.println("3 - Finalizar");
+                opcion=Integer.parseInt(dato.readLine());
+                if(opcion==1){
+                    System.out.println("¿Cuánto desea ingresar?: ");
+                    float ingresar=Integer.parseInt(dato.readLine());
+                    operativa_cuenta(cuenta1,ingresar,opcion);
+                }else if(opcion==2){
+                    System.out.println("¿Cuánto desea retirar?: ");
+                    float retirar=Integer.parseInt(dato.readLine());
+                    operativa_cuenta(cuenta1,retirar,opcion);
+                }else if(opcion==3){
+                    System.out.println("Finalizamos la ejecución");
+                }else{
+                    System.err.println("Opción errónea");
+                }
+/*                switch(opcion){
+                    case 1:
+                        System.out.println("¿Cuánto desea ingresar?: ");
+                        float ingresar=Integer.parseInt(dato.readLine());
+                        operativa_cuenta(cuenta1,ingresar,opcion);
+                        break;
+                    case 2:
+                        System.out.println("¿Cuátno desea retirar?: ");
+                        float retirar=Integer.parseInt(dato.readLine());
+                        operativa_cuenta(cuenta1,retirar,opcion);
+                        break;
+                    case 3:
+                        System.out.println("Finalizamos la ejecución");
+                }
+*/            } catch (IOException ex) {
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }while (opcion!=3);
+        saldoActual = cuenta1.estado();
+        System.out.println("El saldo actual es"+ saldoActual );
+    }
+    public static void operativa_cuenta(CCuenta cuenta1, float cantidad,int opcion){
+        if(opcion==2){
+            try 
+            {
+                cuenta1.retirar(cantidad);
+            } catch (Exception e)
+            {
+                System.out.print("Fallo al retirar");
+            }
+        }else if(opcion==1){
+            try
+            {
+                System.out.println("Ingreso en cuenta");
+                cuenta1.ingresar(cantidad);
+            } catch (Exception e)
+            {
+                System.out.print("Fallo al ingresar");
+            }
+        }
+    }
+}
